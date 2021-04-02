b896aa47f300cccb675c9e324526dfea78cb8925
Darren Govoni <darren@rapidquest.ai>
2021-04-02 05:36:10 -0400

    Example workflow

---
+++ tests/example.py
@@ -0,0 +1,47 @@
+from entangle.process import process
+
+
+@process
+def one():
+    return 1
+
+
+@process
+def two():
+    return 2
+
+
+@process
+def five():
+    return 5
+
+
+@process
+def num(n):
+    return n
+
+
+@process
+def add(a, b):
+    v = int(a) + int(b)
+    print("ADD: *"+str(v)+"*")
+    return v
+
+
+@process
+def subtract(a, b):
+    return int(a) - int(b)
+
+
+result = add(
+            add(
+                num(6),
+                two()
+            ),
+            subtract(
+                five(),
+                two()
+            )
+        )
+
+print(result())
