692d218294a683cba72273ad2ee9ec57d8e29a6a
Darren Govoni <darren@rapidquest.ai>
2021-04-03 16:22:11 -0400

    Added requires

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     packages=['entangle'],
     include_package_data=True,
     python_requires="==3.7.*",
-    install_requires=[],
+    install_requires=['requests'],
     license=about['__license__'],
     zip_safe=False,
     entry_points={
