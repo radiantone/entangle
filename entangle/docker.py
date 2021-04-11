16601cde2b5f7b2bdaf6dd0cab128d90b88a066e
Darren Govoni <darren@rapidquest.ai>
2021-04-10 22:27:55 -0400

    Docker task support

---
+++ entangle/docker.py
@@ -0,0 +1,43 @@
+"""
+docker.py - Module that provides docker support decorators for running tasks inside containers
+"""
+import docker
+import logging
+
+from functools import partial
+
+client = docker.from_env()
+
+
+def docker(function=None,
+           image=None,
+           sleep=0):
+    """
+
+    :param function:
+    :param image:
+    :param sleep:
+    :return:
+    """
+    def decorator(func):
+        def wrapper(f):
+            import inspect
+            import re
+
+            lines = inspect.getsource(f)
+            logging.debug("Running container: {}".format(image))
+            lines = re.sub('@', '#@', lines)
+            name = f.__name__
+            logging.debug(
+                "python -c \"{}\n\nprint({}())\"".format(lines, name))
+            result = client.containers.run(
+                image, "python -c \"{}\nprint({}())\"".format(lines, name))
+            logging.debug(result)
+            return result
+
+        return partial(wrapper, func)
+
+    if function is not None:
+        return decorator(function)
+
+    return decorator
---
+++ examples/dockerexample.py
@@ -0,0 +1,17 @@
+from entangle.docker import docker
+from entangle.process import process
+
+import logging
+logging.basicConfig(
+    format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+
+@process
+@docker(image="tensorflow/tensorflow:latest-gpu")
+def one():
+    import tensorflow as tf
+    return tf.reduce_sum(tf.random.normal([1000, 1000]))
+
+
+o = one()
+print(o())
