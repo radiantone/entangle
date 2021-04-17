2fee923aaf63c608392d9b5e5d2ff1d3df36db96
Darren Govoni <darren@rapidquest.ai>
2021-04-17 14:14:07 -0400

    Added @process and @thread dataflow decorators

--- entangle/dataflow.py
+++ entangle/dataflow.py
@@ -9,6 +9,32 @@ from concurrent.futures import ProcessPoolExecutor
 global _executor
 _executor = None
 
+global processpool, threadpool
+processpool = None
+threadpool = None
+# Add logging to file
+
+
+def process(func, **kwargs):
+
+    def inner(*args, **kwargs):
+        _workflow = func(*args, **kwargs)
+        return _workflow
+
+    inner.thread = False
+    return inner
+
+
+def thread(func, **kwargs):
+
+    def inner(*args, **kwargs):
+        _workflow = func(*args, **kwargs)
+
+        return _workflow
+
+    inner.thread = True
+    return inner
+
 
 class DataflowNode(object):
 
@@ -24,7 +50,12 @@ class DataflowNode(object):
         """
         self.func = func
         self.args = args
+
+        self.thread = func.thread if hasattr(func, 'thread') else True
         self.partial = partial(func, *self.args)
+        self.partial.__name__ = func.__name__
+
+        self.result = kwargs['result'] if 'result' in kwargs else []
         print(self.partial)
         print("{} self.args".format(self.func.__name__), args)
 
@@ -37,9 +68,6 @@ class DataflowNode(object):
         Get my return value
         spawn process for each arg from process pool passing my value to them
         '''
-        print("Calling {}".format(self.partial))
-        print("Self.Args {}".format(self.args))
-        print("Args {}".format(args))
 
         if len(self.args) > 0 and type(self.args[0]) != DataflowNode:
             print("Calling partial")
@@ -51,7 +79,12 @@ class DataflowNode(object):
 
             for arg in args:
                 print('   LAUNCHING: ', arg.func.__name__, result)
-                _executor.submit(arg, result)
+                print('     Thread:', arg.thread)
+                print("Calling {}('{}')".format(arg.func.__name__, result))
+                if arg.thread:
+                    threadpool.submit(arg, result, **{'result': self.result})
+                else:
+                    processpool.submit(arg, result, **{'result': self.result})
         else:
             print("Calling with args {} {}".format(self.func.__name__, *args))
             result = self.func(*args)
@@ -62,8 +95,14 @@ class DataflowNode(object):
 
             for arg in self.args:
                 print('   ARG Type:', type(arg), arg.func)
+                print('     Thread:', arg.thread)
                 print('   LAUNCHING: ', arg.func.__name__, result)
-                _executor.submit(arg, result)
+                print("Calling {}('{}')".format(arg.func.__name__, result))
+
+                if arg.thread:
+                    threadpool.submit(arg, result)
+                else:
+                    processpool.submit(arg, result)
 
         return self
 
@@ -71,7 +110,7 @@ class DataflowNode(object):
 def dataflow(function=None,
              executor=ThreadPoolExecutor,
              maxworkers=3):
-    global _executor
+    global _executor, processpool, threadpool
 
     def decorator(func):
         from functools import partial
@@ -95,10 +134,13 @@ def dataflow(function=None,
         return wrapper(func)
 
     exectype = ThreadPoolExecutor
+
     if executor == 'process':
         exectype = ProcessPoolExecutor
 
-    _executor = exectype(max_workers=maxworkers)
+    #_executor = exectype(max_workers=maxworkers)
+    processpool = ProcessPoolExecutor(max_workers=maxworkers)
+    threadpool = ThreadPoolExecutor(max_workers=maxworkers)
 
     if function is not None:
         return decorator(function)
--- examples/dataflowexample.py
+++ examples/dataflowexample.py
@@ -1,4 +1,7 @@
 from entangle.dataflow import dataflow
+from entangle.dataflow import process
+from entangle.dataflow import thread
+import threading
 
 import logging
 logging.basicConfig(
@@ -6,33 +9,43 @@ logging.basicConfig(
 
 
 @dataflow
+@thread
 def printx(x):
+    print('printx: ', threading.current_thread().name)
     return("X: {}".format(x))
 
 
 @dataflow
+@thread
 def printy(y):
+    print('printy: ', threading.current_thread().name)
     return("Y: {}".format(y))
 
 
 @dataflow
+@thread
 def printz(z):
+    print('printz: ', threading.current_thread().name)
     return("Z: {}".format(z))
 
 
 @dataflow
+@thread
 def echo(e):
+    print('echo: ', threading.current_thread().name)
     return "Echo! {}".format(e)
 
 
-@dataflow(executor='thread', maxworkers=4)
-def emit(a):
+@dataflow(executor='thread', maxworkers=1)
+def emit(a, **kwargs):
     print("Returning from emit")
     return a+"!"
 
 
+results = []
+
 # initial dataflow input
-emit = emit('emit')
+emit = emit('emit', result=results)
 #printx = printx('PREPRINTX')
 
 result = emit(
@@ -43,7 +56,11 @@ result = emit(
     ),
     printy(
         printz()
-    )
+    ),
+    printy()
 )
 
 print(result)
+
+import time
+time.sleep(2)
