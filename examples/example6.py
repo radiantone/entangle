aa9b09c97731ecc8143a7d442b907f4c6d6862d9
Darren Govoni <darren@rapidquest.ai>
2021-04-28 14:39:00 -0400

    Workflow with scheduler and processes

---
+++ examples/example6.py
@@ -0,0 +1,55 @@
+from entangle.process import process
+from entangle.http import request
+from entangle.workflow import workflow
+from entangle.scheduler import scheduler
+
+import logging
+logging.basicConfig(
+    format='%(asctime)s : %(levelname)s : %(message)s', level=logging.DEBUG)
+
+
+@process
+@request(url='https://datausa.io/api/data', method='GET')
+@scheduler(cpus=10, sclass='entangle.scheduler.DefaultScheduler')
+def mydata(data):
+    import json
+    data = json.loads(data)
+    print('My function got the data! ', data)
+    return int(data['data'][0]['Year'])
+
+
+@process
+@scheduler(cpus=10, sclass='entangle.scheduler.DefaultScheduler')
+def two():
+    return 2
+
+
+@process
+@scheduler(cpus=10, sclass='entangle.scheduler.DefaultScheduler')
+def add(a, b):
+    v = int(a) + int(b)
+    print("ADD: *"+str(v)+"*")
+    return v
+
+
+@workflow
+@scheduler(cpus=10, sclass='entangle.scheduler.DefaultScheduler')
+def workflow1():
+    return add(
+        mydata(drilldowns='Nation', measures='Population'),
+        two()
+    )
+
+
+@workflow
+@scheduler(cpus=10, sclass='entangle.scheduler.DefaultScheduler')
+def workflow2(value):
+    return add(
+        value(),
+        two()
+    )
+
+
+result = workflow2(workflow1)
+
+print(result())
