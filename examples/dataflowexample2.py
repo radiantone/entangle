ead911e6533194dae594ed7f03f58c4488e3a6be
Darren Govoni <darren@rapidquest.ai>
2021-04-30 14:16:14 -0400

    Dataflow example using a scheduler

---
+++ examples/dataflowexample2.py
@@ -0,0 +1,81 @@
+import logging
+logging.basicConfig(filename='scheduler.log',
+                    format='%(asctime)s : %(levelname)s : %(message)s', level=logging.DEBUG)
+
+import threading
+import time
+from entangle.dataflow import process
+from entangle.dataflow import dataflow
+from entangle.scheduler import scheduler
+
+
+def triggered(func, result):
+    print("triggered: {} {}".format(func.__name__, result))
+
+
+scheduler_config = {'cpus': 2,
+                    'sclass': 'entangle.scheduler.DefaultScheduler'}
+
+
+@scheduler(**scheduler_config)
+@dataflow(callback=triggered)
+@process
+def printx(x):
+    print('printx: {}'.format(threading.current_thread().name))
+    return("X: {}".format(x))
+
+
+@scheduler(**scheduler_config)
+@dataflow(callback=triggered)
+@process
+def printy(y):
+    print('printy: {}'.format(threading.current_thread().name))
+    return("Y: {}".format(y))
+
+
+@scheduler(**scheduler_config)
+@dataflow(callback=triggered)
+@process
+def printz(z):
+    print('printz: {}'.format(threading.current_thread().name))
+    return("Z: {}".format(z))
+
+
+@scheduler(**scheduler_config)
+@dataflow(callback=triggered)
+@process
+def echo(e):
+    print('echo: {}'.format(threading.current_thread().name))
+    return "Echo! {}".format(e)
+
+
+@scheduler(**scheduler_config)
+@dataflow(executor='thread', callback=triggered, maxworkers=3)
+def emit(a, **kwargs):
+    print('emit: {}'.format(threading.current_thread().name))
+    return a+"!"
+
+
+results = []
+
+# Create the dataflow graph
+flow = emit(
+    printx(
+        printz(
+            echo()
+        )
+    ),
+    printy(
+        printz()
+    ),
+    printy()
+)
+
+# Invoke the dataflow graph with initial input
+flow('emit')
+
+time.sleep(2)
+
+# Call flow again with different input value
+# TODO: This seems to hang
+#flow('HELLO')
