a8aa5f59b0cd64068fa6d1b7c7c072e80a4a5744
Darren Govoni <darren@rapidquest.ai>
2021-05-02 10:47:39 -0400

    Refactored all logging into convenient import module

---
+++ entangle/logging/debug.py
@@ -0,0 +1,5 @@
+import logging
+
+logging.basicConfig(
+    format='%(asctime)s : %(levelname)s : %(message)s', level=logging.DEBUG)
+
---
+++ entangle/logging/file.py
@@ -0,0 +1,4 @@
+import logging
+
+logging.basicConfig(filename='entangle.log',
+                    format='%(asctime)s : %(levelname)s : %(message)s', level=logging.DEBUG)
