0d269bfe1ead57126e60045b4e71c6e3810f15dd
Darren Govoni <darren@rapidquest.ai>
2021-05-07 09:09:55 -0400

    SSH updates

--- entangle/ssh.py
+++ entangle/ssh.py
@@ -118,7 +118,7 @@ def ssh(function=None, **kwargs):
             sourceuuid = "sshsource"+hashlib.md5(uuid4().bytes).hexdigest()
             with open(sourcefile) as source:
                 _source = source.read()
-                _source = re.sub(r"@ssh(user='{}', host='{}'".format(username,
+                _source = re.sub(r"@ssh\(user='{}', host='{}'".format(username,
                                  hostname), "#@ssh(user='{}', host='{}'".format(username,
                                  hostname), _source).strip()
                 with open('{}.py'.format(sourceuuid), 'w') as appsource:
