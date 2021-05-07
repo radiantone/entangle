c270ecf90377170f1a29740ba2e14d18b7f70157
Darren Govoni <darren@rapidquest.ai>
2021-05-07 08:33:07 -0400

    Code cleaning

--- entangle/examples/sshschedulerexample.py
+++ entangle/examples/sshschedulerexample.py
@@ -32,15 +32,21 @@ def three():
     return 3
 
 
+'''
+@ssh instructs entangle to run the function on a remote server. Servers that want to participate with entangle workflows
+must already have shared keys in place to allow passwordless ssh use. Also, the remote machine needs to have a virtualenv
+setup with entangle installed as well.
+'''
 @ssh(user='darren', host='radiant', key='/home/darren/.ssh/id_rsa.pub', python='/home/darren/venv/bin/python')
 @scheduler(**scheduler_config)
 @thread
 def workflow2():
+
     add = add(
         three(),
         two()
     )
-
+    
     return add()
 
 
