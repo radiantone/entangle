e66eeb62858c0024498bca76024e55510de8cdcd
Darren Govoni <darren@rapidquest.ai>
2021-05-11 14:33:19 -0400

    Updated version

--- entangle/__version__.py
+++ entangle/__version__.py
@@ -7,7 +7,7 @@ Information about the current version of the py-package-template package.
 
 __title__ = 'py-entangle'
 __description__ = 'A python native parallel processing framework based on simple decorators.'
-__version__ = '0.0.19'
+__version__ = '0.1.0'
 __author__ = 'Darren Govoni'
 __author_email__ = 'darren@ontrenet.com'
 __license__ = 'MIT'
