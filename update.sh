1f8e2a6b227eebf523d3718f015b04fbd786f110
Darren Govoni <darren@rapidquest.ai>
2021-05-17 08:58:29 -0400

    Removed obsolete files. Renamed.

--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
-read -p "Enter release description: " desc
+python setup.py clean
+read -p "Enter commit message: " desc
 git commit -m "$desc"
 export BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 cmd="sed 's/VERSION/$BRANCH/g'"
